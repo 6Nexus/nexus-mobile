767922466aa4a35b20b406b5c2c40f261a3cb2eb
eduardaguardiao <maria.guardiao@sptech.school>
2025-04-23 00:16:47 -0300

    add: cadastro

---
+++ app/src/main/java/com/example/nexus_mobile/data/model/cadastro/CadastroRequest.kt
@@ -0,0 +1,13 @@
+package com.example.nexus_mobile.data.model.cadastro
+
+data class CadastroRequest(
+    val nome: String,
+    val email: String,
+    val senha: String
+) {
+
+    override fun toString(): String {
+        return "CadastroRequest(nome='$nome', email='$email', senha='$senha')"
+    }
+
+}
---
+++ app/src/main/java/com/example/nexus_mobile/data/model/cadastro/CadastroResponse.kt
@@ -0,0 +1,12 @@
+package com.example.nexus_mobile.data.model.cadastro
+
+class CadastroResponse() {
+    var nome: String = ""
+    var email: String = ""
+
+    override fun toString(): String {
+        return "CadastroResponse(nome='$nome', email='$email')"
+    }
+}
+
+
