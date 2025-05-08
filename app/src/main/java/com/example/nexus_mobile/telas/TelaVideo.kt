6a875be1a5e17febbda56f3fae3737798c3235a7
Gabriel Barreto <gabriel.barreto@sptech.school>
2025-05-08 14:41:03 -0300

    Fix: Video request instance

--- app/src/main/java/com/example/nexus_mobile/telas/TelaVideo.kt
+++ app/src/main/java/com/example/nexus_mobile/telas/TelaVideo.kt
@@ -63,8 +63,8 @@ fun TelaVideo(
     // Garantimos que a matrícula só será verificada quando o idAssociado estiver disponível
     LaunchedEffect(moduloId, idAssociado) {
         if (idAssociado > 0) {
-            viewModel.carregarVideos(moduloId)
-            viewModel.verificarMatricula(idAssociado, cursoId)
+            cursoViewModel.carregarVideos(moduloId)
+//            viewModel.verificarMatricula(idAssociado, cursoId)
         }
     }
 
